7cac8f2da30a726648cd2ce389c1d82053a74d24
sanfenr <sanfenruxi1@163.com>
2017-08-24 16:51:52 +0800

    添加生肖显示

--- Zodiac/ViewController.swift
+++ Zodiac/ViewController.swift
@@ -11,17 +11,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var yearOfBirth: UITextField!
+
+    @IBOutlet weak var image: UIImageView!
+
+    let offset = 4
+
     override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
     override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        yearOfBirth.resignFirstResponder()
+    }
+
+    @IBAction func okTapped(_ sender: Any) {
+        yearOfBirth.resignFirstResponder()
+        if let year = Int(yearOfBirth.text!){
+            var imageIndex = (year - offset) % 12
+            image.image = UIImage(named: String(imageIndex))
+        }
+    }
 
 
 }
